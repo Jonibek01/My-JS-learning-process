84b2eeff5490fa4f33cf624ac8351722ff727768
Jonibek01 <60321513+Jonibek01@users.noreply.github.com>
2021-02-12 23:09:04 +0500

    added js files and bootstrap files

--- JS-currency-converter/js/app.js
+++ JS-currency-converter/js/app.js
@@ -1,1 +1,29 @@
+// firtly should give correct names
 
+var elExchangeForm = document.querySelector('.Exchange-form');
+var elInputSumm = document.querySelector('.input-summ');
+var elCurrencySelect = document.querySelector('.currency-select')
+var elResultWrapper = document.querySelector('.result-wrapper');
+var elResult = document.querySelector('.result');
+
+
+elExchangeForm.addEventListener('submit' , function(evt){
+    evt.preventDefault();
+
+// take the input change to number, trim and fixed
+    var summ = parseFloat(elInputSumm.value.trim(), 10);
+
+    // if (isNaN(summ)) {
+    //     console.log('plz, enter number');
+    //     return;
+    // }
+// take the user selection
+    var select = parseFloat(elCurrencySelect.value.trim(), 10);
+// multiply the input by selecctions
+    var result = (summ * select).toFixed(2);
+    console.log(result)
+// show the result in output
+    elResultWrapper.classList.remove('d-none');
+    elResult.textContent = result;
+
+})
