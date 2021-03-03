114489aedbaebe42ed564f019b2357e5fc49cf64
Jonibek01 <60321513+Jonibek01@users.noreply.github.com>
2021-03-04 00:54:34 +0500

    Yes/No shopping list, can  try to see on console

--- Js-shopping-list-yes-no/js/app.js
+++ Js-shopping-list-yes-no/js/app.js
@@ -3,6 +3,7 @@ var elAddFormListProduct = document.querySelector('.add-shopping-list');
 var elAddFormListProductInput = document.querySelector('.add-shopping-list-input');
 var elCheckFormListProduct = document.querySelector('.check-shopping-list');
 var elCheckFormListProductInput = document.querySelector('.check-shopping-list-input');
+var elShopList = document.querySelector('.shop-list')
 
 elAddFormListProduct.addEventListener('submit', function(evt){
     evt.preventDefault();
@@ -10,5 +11,32 @@ elAddFormListProduct.addEventListener('submit', function(evt){
     shoppingList.push(elAddFormListProductInput.value);
     elAddFormListProductInput.value = '';
     console.log(shoppingList);
+
+    // for (var product of elShopList){
+    //     var elProduct = document.createElement('li');
+    //     elProduct.classList.add('list-group-item');
+    //     elProduct.textContent = product;
+    // }
+    // elShopList.appendChild(elProduct);
+})
+
+elCheckFormListProduct.addEventListener('submit', function(evt){
+    evt.preventDefault(); 
+
+    // making much easier to take the var of this input value if its big
+    var isThereAnything = shoppingList.includes(elCheckFormListProductInput.value);
+
+    if (isThereAnything) {
+        console.log('We already wrote that');
+
+    } else {
+        console.log('Thanks for remainding');
+        // taking the value from check input and putting to add input 
+        elAddFormListProductInput.value = elCheckFormListProductInput.value;
+        // remaining nothing into check input
+        elCheckFormListProductInput.value = '';
+        // giving focus to the add input
+        elAddFormListProductInput.focus()
+    }
 })
 
